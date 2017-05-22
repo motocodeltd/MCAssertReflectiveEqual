04ba31cdf125ced5c6df6663489289131125f046
Stefanos Zachariadis <stefanos@zachariadis.net>
2017-05-22 08:49:08 +0100

    indentation

--- MCAssertReflectiveEqual/Classes/MCAssertReflectiveEqual.swift
+++ MCAssertReflectiveEqual/Classes/MCAssertReflectiveEqual.swift
@@ -3,7 +3,7 @@ import XCTest
 
 internal typealias MCNSObjectEqualsFunction = (NSObject, NSObject, String, StaticString, UInt) -> Void
 
-private let NSObjectEqualsFunction:MCNSObjectEqualsFunction = { (expected, actual, message, file, line) in
+private let NSObjectEqualsFunction: MCNSObjectEqualsFunction = { (expected, actual, message, file, line) in
     XCTAssertEqual(expected, actual, message, file: file, line: line)
 }
 
@@ -22,7 +22,7 @@ private let failFunction: MCFailFunction = { (message, file, line) in
 public class Matcher {
     fileprivate let type: Any
     fileprivate let comparator: (Any, Any) -> Bool
-    
+
     init<T>(type: T.Type, comparator: @escaping (T, T) -> Bool) {
         self.type = type;
         self.comparator = { (a, b) -> Bool in
@@ -31,29 +31,28 @@ public class Matcher {
     }
 }
 
-public func matcherFor<T>(_ type: T.Type, _ comparator: @escaping (_ expected: T, _ actual: T) -> Bool) -> Matcher{
+public func matcherFor<T>(_ type: T.Type, _ comparator: @escaping (_ expected: T, _ actual: T) -> Bool) -> Matcher {
     return Matcher(type: type, comparator: comparator)
 }
 
-
 public func MCAssertReflectiveEqual<T>(_ expected: T, _ actual: T,
-                                    customMatchers: [Matcher] = [],
-                                    file: StaticString = #file,
-                                    line: UInt = #line) {
+                                       customMatchers: [Matcher] = [],
+                                       file: StaticString = #file,
+                                       line: UInt = #line) {
     internalMCAssertReflectiveEqual(expected, actual, customMatchers: customMatchers, file: file, line: line)
 }
 
 internal func internalMCAssertReflectiveEqual<T>(_ expected: T, _ actual: T,
-                                              customMatchers: [Matcher],
-                                    file: StaticString = #file,
-                                    line: UInt = #line,
-                                    nsObjectCheckFunction: MCNSObjectEqualsFunction = NSObjectEqualsFunction,
-                                    optionalStringEqualsFunction: MCOptionalStringEqualsFunction = optionalStringEqualsFunction,
-                                    failFunction: MCFailFunction = failFunction) {
+                                                 customMatchers: [Matcher],
+                                                 file: StaticString = #file,
+                                                 line: UInt = #line,
+                                                 nsObjectCheckFunction: MCNSObjectEqualsFunction = NSObjectEqualsFunction,
+                                                 optionalStringEqualsFunction: MCOptionalStringEqualsFunction = optionalStringEqualsFunction,
+                                                 failFunction: MCFailFunction = failFunction) {
     var expectedVisited = Set<ObjectIdentifier>()
     var actualVisited = Set<ObjectIdentifier>()
-    
-    let matchersByType:[String: (Any, Any) -> Bool] = customMatchers.reduce([String:(Any, Any) -> Bool]()) { (result, matcher) in
+
+    let matchersByType: [String: (Any, Any) -> Bool] = customMatchers.reduce([String: (Any, Any) -> Bool]()) { (result, matcher) in
         dump(matcher)
         var mutableResult = result
         var desc = String()
@@ -61,14 +60,14 @@ internal func internalMCAssertReflectiveEqual<T>(_ expected: T, _ actual: T,
         mutableResult[desc] = matcher.comparator
         return mutableResult
     }
-    
+
     MCAssertReflectiveEqual(expected, actual, expectedVisited: &expectedVisited, actualVisited: &actualVisited,
-                            expectedDescription: "", actualDescription: "", depth: 0,
-                            matchersByType: matchersByType,
-                            file: file, line: line,
-                            nsObjectCheckFunction: nsObjectCheckFunction,
-                            optionalStringEqualsFunction: optionalStringEqualsFunction,
-                            failFunction: failFunction)
+            expectedDescription: "", actualDescription: "", depth: 0,
+            matchersByType: matchersByType,
+            file: file, line: line,
+            nsObjectCheckFunction: nsObjectCheckFunction,
+            optionalStringEqualsFunction: optionalStringEqualsFunction,
+            failFunction: failFunction)
 }
 
 private func appendItemDescription(_ item: Any, previousDescription: String, depth: Int) -> String {
@@ -77,9 +76,9 @@ private func appendItemDescription(_ item: Any, previousDescription: String, dep
     }).reduce("") { (old, new) -> String in
         return old.appending(new)
     }
-    
+
     let initialNewLine = depth == 0 ? "" : "\n"
-    
+
     return "\(initialNewLine)\(tabs) \(item)"
 }
 
@@ -96,71 +95,69 @@ private func MCAssertReflectiveEqual(_ expected: Any,
                                      nsObjectCheckFunction: MCNSObjectEqualsFunction,
                                      optionalStringEqualsFunction: MCOptionalStringEqualsFunction,
                                      failFunction: MCFailFunction) {
-    
+
     let expectedDescription = appendItemDescription(expected, previousDescription: expectedDescription, depth: depth)
     let actualDescription = appendItemDescription(actual, previousDescription: actualDescription, depth: depth)
     let expectedMirror = Mirror(reflecting: expected)
     let actualMirror = Mirror(reflecting: actual)
-    
+
     guard expectedMirror.subjectType == actualMirror.subjectType else {
-        failFunction( "Types not the same expected: \(expectedDescription) is a \(expectedMirror.subjectType) \ngot:\n\(actualDescription) which is a \(actualMirror.subjectType)", file, line)
+        failFunction("Types not the same expected: \(expectedDescription) is a \(expectedMirror.subjectType) \ngot:\n\(actualDescription) which is a \(actualMirror.subjectType)", file, line)
         return
     }
-    
+
     var typeAsString = String()
     dump(expectedMirror.subjectType, to: &typeAsString)
-    
+
     if let matcher = matchersByType[typeAsString] {
-        if(!matcher(expected, actual)) {
+        if (!matcher(expected, actual)) {
             failFunction("\(expectedDescription)\nnot equal to\n\(actualDescription) using custom matcher", file, line)
         }
         return
     }
-    
+
     let expectedAsObject = expected as AnyObject
     let actualAsObject = actual as AnyObject
-    
-    if(expectedAsObject === actualAsObject) {
+
+    if (expectedAsObject === actualAsObject) {
         return
     }
-    
+
     var expectedChildren = expectedMirror.children
     var actualChildren = actualMirror.children
-    
+
     guard expectedChildren.count == actualChildren.count else {
         failFunction("\(expectedDescription) has \(expectedChildren.count) but \(actualDescription) has \(actualChildren.count)", file, line)
         return
     }
-    
-    if(expectedChildren.count == 0) {
+
+    if (expectedChildren.count == 0) {
         if let expectedNsObj = expected as? NSObject, let actualNsObj = actual as? NSObject {
             nsObjectCheckFunction(expectedNsObj, actualNsObj, "\(expectedDescription)\nnot equal to\n\(actualDescription)", file, line)
-        } else if(expectedMirror.displayStyle == .struct || expectedMirror.displayStyle == .class) {
+        } else if (expectedMirror.displayStyle == .struct || expectedMirror.displayStyle == .class) {
             return
-        } else if(expectedMirror.displayStyle == .enum) {
+        } else if (expectedMirror.displayStyle == .enum) {
             optionalStringEqualsFunction(String(describing: expected), String(describing: actual), "\(expectedDescription)\nnot equal to\n\(actualDescription)", file, line)
-        }
-        else if(expectedMirror.description.contains("->")) {
+        } else if (expectedMirror.description.contains("->")) {
             print("ignoring closures in \n\(expectedDescription)\nand\n\(actualDescription)")
-        }
-        else {
+        } else {
             failFunction("cannot compare\n\(expectedDescription)\n\(actualDescription)", file, line)
         }
     } else {
-        while(!expectedChildren.isEmpty) {
+        while (!expectedChildren.isEmpty) {
             let expectedChild = expectedChildren.popFirst()!
             let actualChild = actualChildren.popFirst()!
-            
+
             let canHoldChildrenByReference = expectedMirror.displayStyle == .class
 
-            if(canHoldChildrenByReference) {
+            if (canHoldChildrenByReference) {
                 let expectedChildAsObject = expectedChild.value as AnyObject
                 let actualChildAsObject = actualChild.value as AnyObject
                 let expectedHasBeenVisited = !expectedVisited.insert(ObjectIdentifier(expectedChildAsObject)).inserted
                 let actualHasBeenVisited = !actualVisited.insert(ObjectIdentifier(actualChildAsObject)).inserted
-                
-                if(expectedHasBeenVisited || actualHasBeenVisited) {
-                    if(expectedHasBeenVisited == actualHasBeenVisited) {
+
+                if (expectedHasBeenVisited || actualHasBeenVisited) {
+                    if (expectedHasBeenVisited == actualHasBeenVisited) {
                         print("\(expectedDescription)\nand\(actualDescription)\n are matching looping objects")
                         return
                     } else {
@@ -169,21 +166,21 @@ private func MCAssertReflectiveEqual(_ expected: Any,
                     }
                 }
             }
-            
-            
+
+
             optionalStringEqualsFunction(expectedChild.label, actualChild.label, "\(expectedDescription): \(String(describing: expectedChild.label)) not equal to \n\(actualDescription): \(String(describing: actualChild.label))", file, line)
-            
+
             MCAssertReflectiveEqual(expectedChild.value, actualChild.value,
-                                    expectedVisited: &expectedVisited, actualVisited: &actualVisited,
-                                    expectedDescription: expectedDescription,
-                                    actualDescription: actualDescription,
-                                    depth: depth + 1,
-                                    matchersByType: matchersByType,
-                                    file: file, line: line,
-                                    nsObjectCheckFunction: nsObjectCheckFunction,
-                                    optionalStringEqualsFunction: optionalStringEqualsFunction,
-                                    failFunction: failFunction)
-            if(canHoldChildrenByReference) {
+                    expectedVisited: &expectedVisited, actualVisited: &actualVisited,
+                    expectedDescription: expectedDescription,
+                    actualDescription: actualDescription,
+                    depth: depth + 1,
+                    matchersByType: matchersByType,
+                    file: file, line: line,
+                    nsObjectCheckFunction: nsObjectCheckFunction,
+                    optionalStringEqualsFunction: optionalStringEqualsFunction,
+                    failFunction: failFunction)
+            if (canHoldChildrenByReference) {
                 _ = expectedVisited.remove(ObjectIdentifier(expectedChild.value as AnyObject))
                 _ = actualVisited.remove(ObjectIdentifier(actualChild.value as AnyObject))
             }
